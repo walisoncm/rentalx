1fde28cc1c1d85e650c6fe9793396ccc3912c679
Walison Magalhães <walison.magalhaes@atsinformatica.com.br>
2022-03-29 19:16:04 -0300

    SOLID

---
+++ src/repositories/CategoriesRepository.ts
@@ -0,0 +1,31 @@
+import { Category } from "../model/Category";
+import {
+  ICategoriesRepository,
+  ICreateCategoryDTO,
+} from "./ICategoriesRepository";
+
+class CategoriesRepository implements ICategoriesRepository {
+  private categories: Category[];
+
+  constructor() {
+    this.categories = [];
+  }
+
+  create({ name, description }: ICreateCategoryDTO) {
+    const category = new Category();
+
+    Object.assign(category, { name, description });
+
+    this.categories.push(category);
+  }
+
+  list(): Category[] {
+    return this.categories;
+  }
+
+  findByName(name: string): Category {
+    return this.categories.find((category) => category.name === name);
+  }
+}
+
+export { CategoriesRepository };
