377cda69e7378d25f87115e5bfc37dbae0eca42d
Walison Magalhães <wcm-@live.com>
2022-03-31 13:45:23 -0300

    Start documentation

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,15 @@
 import express from "express";
+import swaggerUi from "swagger-ui-express";
 
 import { router } from "./routes";
+import swaggerDocument from "./swagger.json";
 
 const app = express();
 
 app.use(express.json());
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 app.use(router);
 
 app.listen(3333, () => console.log("Server started on port 3333"));
